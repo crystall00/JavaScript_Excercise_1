1e06693c3b094e40ba131fb3c65b3555845b8425
Anton Markaj <antonimarkaj@gmail.com>
2018-09-10 16:09:02 +0200

    Removing Comments

---
+++ Ex3_CheckPangramm.js
@@ -0,0 +1,11 @@
+function isPangramm(string_input) {
+    let allLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+    let stringLetters = string_input.toLowerCase().split("");
+
+
+    for ( var i = 0; i < letters.length; i++){
+       //comparing the 2 vectors. Not done yet.
+    }
+}
+
+console.log(isPangramm("The quick brown fox jumps over the lazy dog"));
